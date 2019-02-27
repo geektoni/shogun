1012ad453d858eb1657b6a12c586915b1c390020
Giovanni De Toni <giovanni.det@gmail.com>
2019-02-27 16:52:52 +0100

    Add new ParameterObserver which just print informations to screen.

---
+++ src/shogun/lib/parameter_observers/ParameterObserverLogger.cpp
@@ -0,0 +1,25 @@
+/*
+* Written (W) 2019 Giovanni De Toni
+*/
+
+#include "ParameterObserverLogger.h"
+#include <shogun/io/SGIO.h>
+#include <shogun/lib/type_case.h>
+
+using namespace shogun;
+
+ParameterObserverLogger::ParameterObserverLogger() {}
+
+ParameterObserverLogger::ParameterObserverLogger(std::vector<std::string> &parameters)
+		: ParameterObserverInterface(parameters) {}
+
+void ParameterObserverLogger::on_next(const TimedObservedValue &value) {
+
+	auto f_print = [&value](auto _disp_value) {
+		SG_SPRINT("[%l] Received %s", convert_to_millis(value.second), _disp_value)
+	};
+
+	// Dispatch the value and print it
+	sg_any_dispatch(value.first.get_value(), sg_all_typemap, f_print);
+
+}
---
+++ src/shogun/lib/parameter_observers/ParameterObserverLogger.h
@@ -0,0 +1,27 @@
+/*
+* Written (W) 2019 Giovanni De Toni
+*/
+
+#ifndef SHOGUN_PARAMETEROBSERVERLOGGER_H
+#define SHOGUN_PARAMETEROBSERVERLOGGER_H
+
+#include <shogun/lib/parameter_observers/ParameterObserverInterface.h>
+
+namespace shogun {
+	class ParameterObserverLogger : public ParameterObserverInterface {
+
+	public:
+		ParameterObserverLogger();
+		ParameterObserverLogger(std::vector<std::string> &parameters);
+
+		/**
+		 * This will print on screen everything it receives
+		 * @param value the observed value
+		 */
+		virtual void on_next(const TimedObservedValue &value);
+
+	};
+
+}
+
+#endif //SHOGUN_PARAMETEROBSERVERLOGGER_H
